0c8c0a0f65eda9185c309056dc79a2e07e70da64
Jess santos <jessicatavares1003@gmail.com>
2025-09-01 09:45:05 -0300

    Update Supertrunfonovato.c

--- Supertrunfonovato.c
+++ Supertrunfonovato.c
@@ -67,7 +67,10 @@ int main() {
     printf("Área: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos turísticos: %d\n", pontos1);
-	
+	densidade1 = populacao1 / area1;
+    pibPerCapita1 = pib1 / populacao1;
+    printf("Densidade populacional:%.2f\n", densidade1);
+    printf("Pib per capito:%.2f\n", pibPerCapita1);
    
    //carta2
    
@@ -79,7 +82,109 @@ int main() {
     printf("Área: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Pontos turísticos: %d\n", pontos2);
-  
-    return 0;
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = pib2 / populacao2;
+    printf("Densidade populacional:%.2f\n", densidade2);
+    printf("Pib per capito:%.2f\n", pibPerCapita2);
 
-}
+    //calculos
+   densidade1 = (float)populacao1 / area1;
+   pibPerCapita1 = pib1 / (float)populacao1;
+   densidade2 = (float)populacao2 / area2;
+   pibPerCapita2 = pib2 / (float)populacao2;
+   
+   printf("\n--------MENU ---------\n");
+   
+   // Comparacao entre a carta 1 e 2
+	 printf("Menu = 1- populacao\n");
+     printf("Menu = 2- Area\n");
+     printf("Menu = 3- Pib\n");
+     printf("Menu = 4- pontos turisticos\n");
+     printf("Menu = 5- Densidade demografica\n");
+     printf("Qual opcao deseja:");
+	 scanf("%d", &opcao);
+    
+     printf("Qual pais deseja incluir no jogo:\n");
+      scanf("%c", &pais1);
+    printf("Qual outro pais deseja  incluir no jogo:\n");
+     scanf("%c", &pais2);
+	 
+	 switch (opcao){
+     
+     case 1:
+     	printf("Voce escolheu a populacao:\n");
+       	if (populacao1>populacao2) {
+		 printf("EBA, carta 1 foi a ganhadora!!\n");
+		 printf("o pais vencedor foi:%c", pais1);
+		 } else if(populacao2>populacao1){
+	        printf("EBA, carta 2 foi ganhou!\n");
+		    printf("o pais vencedor foi:%c", pais2);
+		 } else{
+		   printf("Poxa, ambos ficaram empatadas!\n");
+	      }
+          break;
+	 case 2:
+	   printf("Voce escolheu a Area\n");
+	   if(area1>area2) {
+	   printf("A carta 1 ganhou!\n");
+	   printf("o pais vencedor foi:%s", pais1);
+	   } else if (area2>area1){
+	   printf("A carta 2 foi a vencedora\n");
+       printf("o pais vencedor foi:%s", pais2);
+	   } else{
+	  printf("Deu empate a comparacao\n");
+	   }
+	   break;
+	case 3:
+	    printf("Voce escolheu o Pib\n");
+	    if(pib1>pib2) {
+	    printf("O carta 1 ganhou!\n");
+	   printf("o pais vencedor foi:%c", pais1);
+	   } else if (pib2>pib1){
+	   printf("o carta 2 foi a vencedora\n");
+       printf("o pais vencedor foi:%c", pais2);
+	   } else{
+	  printf("Deu empate a comparaca\nn");
+	    break;
+	     }
+	 case 4:
+	    printf("Voce escolheu o pontos turisticos\n");
+	    if(pontos1>pontos2) {
+	    printf("O carta 1 ganhou!\n");
+	   printf("o pais vencedor foi:%c", pais1);
+	   } else if (pontos2>pontos1){
+	   printf("o carta 2 foi a vencedora\n");
+       printf("o pais vencedor foi:%c", pais2);
+	   } else{
+	  printf("Deu empate a comparacao\n");
+	  }
+	   break; 
+		case 5:
+	    printf("Voce escolheu a Densidade\n");
+	    if(densidade1<densidade2) {
+	    printf("O carta 1 ganhou!\n");
+	    printf("o pais vencedor foi:%c", pais1);
+	   } else if (densidade2>densidade1){
+	   printf("o pais vencedor foi:%c", pais2);
+	   printf("o carta 2 foi a vencedora\n");
+       } else{
+	  printf("Deu empate a comparacao\n");
+        }
+     break;
+   }
+	    return 0;
+        }
+	    
+	    
+
+
+
+
+
+
+
+
+
+
+
+   
